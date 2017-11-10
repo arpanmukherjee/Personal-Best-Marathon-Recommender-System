4a2d5f969a1f07abbb44cb73c417691f510c7302
arpanmukherjee <player.arpan@gmail.com>
2017-11-10 12:41:23 +0000

    Splitted dataset

--- TestData/dataConverter.py
+++ TestData/dataConverter.py
@@ -2,14 +2,25 @@ def convert(data):
     temp = data.split('\t')
     t_time = int(float(temp[3]))
     temp[3] = str(t_time)
+    t_temp = ""
+    i = 0
+    while i<len(temp)-1:
+	t_temp += (str(temp[i])+"\t")
+	i += 1
+    t_temp += str(temp[i])
+    return t_temp
+	
+	
 
 
-f1 = open('testData.txt','w')
-f2 = open('trainData.txt', 'w')
+f1 = open('trainData.txt', 'w')
+f2 = open('testData.txt','w')
 lines = [line.rstrip('\n') for line in open('updatedData.txt')]
 per_dict = {}
 for i in lines:
     temp = i.split('\t')
+    if temp[3]=='-':
+        continue
     try:
         if temp[0] in per_dict.keys():
             per_dict[temp[0]].append(i)
@@ -24,15 +35,8 @@ for i in lines:
 for key in per_dict:
     if len(per_dict[key]) < 3:
         continue
-    for j in range(len(per_dict[key])):
-        temp = per_dict[key][j].split('\t')
-        t_time = int(float(temp[3]))
-        temp[3] = str(t_time)
-        t_temp = ""
-        i = 0
-        while i<len(temp)-1:
-            t_temp += (str(temp[i])+"\t")
-            i += 1
-        t_temp += str(temp[i])
-        f1.write(str(t_temp)+'\n')
+    for j in range(len(per_dict[key])-1):
+        f1.write(convert(per_dict[key][j])+"\n")
+    f2.write(convert(per_dict[key][j])+"\n");
 f1.close()
+f2.close()
